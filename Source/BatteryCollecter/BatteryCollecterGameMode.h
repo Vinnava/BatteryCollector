d719a48ef0f7744185d2e9cf1ba2feec3e6c019a
Vinnava <109844032+Vinnava@users.noreply.github.com>
2024-11-25 23:08:05 +0530

    Initial

---
+++ Source/BatteryCollecter/BatteryCollecterGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BatteryCollecterGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ABatteryCollecterGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ABatteryCollecterGameMode();
+};
+
+
+
