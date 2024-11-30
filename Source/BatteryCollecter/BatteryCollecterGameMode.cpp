97d1d3a83bdf94ee9b56f43efd275a746844f304
Vinnava <109844032+Vinnava@users.noreply.github.com>
2024-11-30 22:17:44 +0530

    Added Basic spawn Code

--- Source/BatteryCollecter/BatteryCollecterGameMode.cpp
+++ Source/BatteryCollecter/BatteryCollecterGameMode.cpp
@@ -4,12 +4,42 @@
 #include "BatteryCollecterCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
+
 ABatteryCollecterGameMode::ABatteryCollecterGameMode()
 {
+	PrimaryActorTick.bCanEverTick = true;
+	
 	// set default pawn class to our Blueprinted character
 	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
 	}
+}
+
+
+void ABatteryCollecterGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
+void ABatteryCollecterGameMode::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	
+}
+
+
+void ABatteryCollecterGameMode::SpawnPlayerRechage()
+{
+	float RandX = FMath::RandRange(Spawn_X_Min, Spawn_X_Max);
+	float RandY = FMath::RandRange(Spawn_Y_Min, Spawn_Y_Max);
+
+	FVector SpawnPosition(RandX, RandY,Spawn_Y_Min);
+	FRotator SpawnRotation(0.0f, 0.0f, 0.0f);
+
+	GetWorld()->SpawnActor(PlayerRechage, &SpawnPosition, &SpawnRotation);
+	
 }
--- Source/BatteryCollecter/BatteryCollecterGameMode.h
+++ Source/BatteryCollecter/BatteryCollecterGameMode.h
@@ -11,6 +11,28 @@ class ABatteryCollecterGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
 
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+
+	float Spawn_Z = 500.0f;
+	
+	UPROPERTY(EditAnywhere)
+		TSubclassOf<APawn> PlayerRechage;
+
+	UPROPERTY(EditAnywhere)
+		float Spawn_Y_Min;
+
+	UPROPERTY(EditAnywhere)
+		float Spawn_Y_Max;
+
+	UPROPERTY(EditAnywhere)
+		float Spawn_X_Min;
+
+	UPROPERTY(EditAnywhere)
+		float Spawn_X_Max;
+
+	void SpawnPlayerRechage();
+
 public:
 	ABatteryCollecterGameMode();
 };
